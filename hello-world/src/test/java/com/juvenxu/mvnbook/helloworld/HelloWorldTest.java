90e526713a9836316a728b643506109919d11e5d
YanLin <chriszt@126.com>
2021-03-09 02:54:07 +0800

    first commit

---
+++ hello-world/src/test/java/com/juvenxu/mvnbook/helloworld/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package com.juvenxu.mvnbook.helloworld;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSayHello () {
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHello();
+        assertEquals("Hello Maven", result);
+    }
+
+}
